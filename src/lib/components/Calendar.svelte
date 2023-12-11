c6de112a8c926fb2d9734db6503b0640eeca5c8d
jensstat <jensbergjensen@gmail.com>
2023-12-11 21:43:20 +0100

    jeepp

--- src/lib/components/Calendar.svelte
+++ src/lib/components/Calendar.svelte
@@ -1,6 +1,7 @@
 <script>
     import Day from "./Day.svelte";
     let days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+    
     const quotes = [
   "Why was the snowman looking through the carrots? He was picking his nose!",
   "What do you get if you cross a snowman and a vampire? Frostbite!",
@@ -28,13 +29,45 @@
   "What did Santa say to the smoker? Please don't smoke, it's bad for my elf!"
 ];
 
-    let currentDay = 0;
-</script>
 
+    let currentDay = 0;
+    function isClickable(day) {
+        const currentDate = new Date();
+
+        // Set the target date to December 1st of the current year
+        const targetDate = new Date(currentDate.getFullYear(), 11, day);
+
+        // Check if the target date is today or in the past
+        return currentDate >= targetDate;
+    }
+</script>
+<div class="calendar flex md:w-11/12 justify-center flex-wrap gap-2">
+    {#if currentDay == 0}
+        {#each days as day}
+            <div class="days" class:clickable={isClickable(day)} on:click={() => {if(isClickable(day)) currentDay = day; console.log(currentDay); console.log(day)}}>
+                <Day day={day} />
+            </div>
+        {/each}
+    {/if}
+
+    {#if currentDay != 0}
+        <div class="quotepage w-full flex flex-col items-center md:block">
+            <button class="back-button mt-5 mb-5 md:mt-0 md:mb-0 md:ml-14" on:click={() => {
+                currentDay = 0;
+            }}><span>Tilbake</span></button>
+            <div class="flex justify-center ">
+                <div class="quote flex dark:text-white mb-7 mt-7">
+                    <span class="quote_text text-4xl md:text-8xl/loose">{quotes[currentDay]}</span>
+                </div>
+            </div>
+        </div>
+    {/if}
+</div>
+<!--
 <div class="calendar flex md:w-11/12 justify-center flex-wrap gap-2">
 {#if currentDay == 0}
 {#each days as day}
-<div on:click={() => {currentDay = day; console.log(currentDay)}}>
+<div on:click={() => {if(isClickable(day)) currentDay = day; console.log(currentDay); console.log(day)}}>
     <Day day={day} />
 
 </div>
@@ -47,14 +80,15 @@
     <button class="back-button mt-5 mb-5 md:mt-0 md:mb-0 md:ml-14" on:click={() => {
         currentDay = 0;
     }}><span>Tilbake</span></button>
-    <div class="flex items-center "> 
-        <div class="quote flex  dark:text-white mb-11 mt-11">
+    <div class="flex justify-center "> 
+        <div class="quote flex dark:text-white mb-7 mt-7">
             <span class="quote_text text-4xl md:text-8xl/loose">{quotes[currentDay]}</span>
         </div>
     </div>
 </div>
 {/if}
 </div>
+-->
 <style>
     .back-button {
         background: rgb(101 163 13 / 1);
@@ -63,12 +97,16 @@
         padding: 5px 10px;
         border-radius: 10px;
     }
-    
+    .quote_text {
+    text-align: center;
+    transform: scale(0.5);
+    animation: scale 7s forwards cubic-bezier(0.5, 1, 0.89, 1);
+  }
 
     .quote_text{
         text-align: center;
         transform: scale(0.5);
-  animation: scale 3s forwards cubic-bezier(0.5, 1, 0.89, 1);
+  animation: scale 7s forwards cubic-bezier(0.5, 1, 0.89, 1);
 }
 @keyframes scale {
   100% {
@@ -76,6 +114,9 @@
   }
     }
 
-    
+    .days:not(.clickable) {
+    pointer-events: none;
+    opacity: 0.5;
+  }
 
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,40 @@
 <script>
-import Header from "$lib/components/Header.svelte";
-import Calendar from "$lib/components/Calendar.svelte";
-import Icon from "$lib/assets/tree.svg"
-let screenSize;
-</script>
-
-
-
-<svelte:window bind:innerWidth={screenSize} />
-    <div class="flex flex-col pt-5 md:pt-0 w-screen items-center h-full md:h-screen" >
+    import Header from "$lib/components/Header.svelte";
+    import Calendar from "$lib/components/Calendar.svelte";
+    import Icon from "$lib/assets/tree.svg";
+    
+    let screenSize;
+  </script>
+  
+  <svelte:window bind:innerWidth={screenSize} />
+  
+  <div class="flex flex-col min-h-screen items-center pt-5 md:pt-0">
     <div class="w-8/12">
-        <Header />
+      <Header />
     </div>
-
-    <div class="flex flex-col md:flex-row md:self-center md:w-11/12 lg:items-center">
-        {#if screenSize < 768}
-        <img src="{Icon}" class="filter_white self-center h-32 mb-5 dark:invert dark:hue-rotate-180" alt="tree">
-        {:else}
-        <img src="{Icon}" class="filter_white h-32 dark:invert dark:hue-rotate-180" alt="tree">
-        {/if}
-        <Calendar />
-        {#if screenSize > 768}
-        <img src="{Icon}" class="filter_white h-28 dark:invert dark:hue-rotate-180" alt="tree">
-        {/if}
-        {#if screenSize < 768}
-        <img src="{Icon}" class="filter_white self-center  mt-12 h-32 mb-5 dark:invert dark:hue-rotate-180" alt="tree">
-        {/if}
-
-    </div>  
-</div>
-<style>
-    .filter_white{
-        opacity: 0.5;
+  
+    <div class="flex flex-col md:flex-row md:w-11/12 items-center">
+      {#if screenSize < 768}
+        <img src="{Icon}" class="filter_white self-center h-32 mb-5 dark:invert dark:hue-rotate-180" alt="tree" />
+      {:else}
+        <img src="{Icon}" class="filter_white h-32 dark:invert dark:hue-rotate-180" alt="tree" />
+      {/if}
+  
+      <Calendar />
+  
+      {#if screenSize > 768}
+        <img src="{Icon}" class="filter_white h-28 dark:invert dark:hue-rotate-180" alt="tree" />
+      {/if}
+  
+      {#if screenSize < 768}
+        <img src="{Icon}" class="filter_white self-center mt-12 h-32 mb-5 dark:invert dark:hue-rotate-180" alt="tree" />
+      {/if}
+    </div>
+  </div>
+  
+  <style>
+    .filter_white {
+      opacity: 0.5;
     }
-</style>
+  </style>
+  
